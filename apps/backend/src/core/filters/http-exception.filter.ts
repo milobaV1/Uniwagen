5e142b32ff94dde7361c2f4a4a41e45c3150c7bf
XANDER27092005 <michael.iloba@pau.edu.ng>
2025-04-11 14:50:44 +0100

    feat: implemented error handling

---
+++ apps/backend/src/core/filters/http-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : exception.message || 'Internal server error';
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,20 +2,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './core/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('CampusCart APIs')
-  .setDescription('CampusCart is a PWA for students at PAU to buy and sell items within the campus. It offers a simple, secure platform for one-time sales, with a user-friendly interface that works seamlessly on mobile and desktop. Connect, list, and sell items easily!')
-  .setVersion('1.0')
-  .addServer('http://localhost:3001', 'Local Server')
-  .build();
+    .setTitle('CampusCart APIs')
+    .setDescription(
+      'CampusCart is a PWA for students at PAU to buy and sell items within the campus. It offers a simple, secure platform for one-time sales, with a user-friendly interface that works seamlessly on mobile and desktop. Connect, list, and sell items easily!',
+    )
+    .setVersion('1.0')
+    .addServer('http://localhost:3001', 'Local Server')
+    .build();
 
   const documentFactory = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
